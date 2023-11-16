49bd018c70f295729cc90b80c17f28c0328ab666
Michal Rydz <mrydz@cleeng.com>
2023-11-16 07:04:46 +0100

    add styling examples

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,15 @@ Config.setPublisher(Cleeng.publisherId);
 Config.setOffer(Cleeng.offerId);
 Config.setVisibleAdyenPaymentMethods(["card", "applepay"]);
 Config.setHidePayPal();
+Config.setTheme({
+  backgroundColor: "#2a174a",
+  loaderColor: "#cccccc",
+  successColor: 'rgb(239,68,68)',
+  errorColor: "red",
+  fontColor: "#ffffff",
+  cardColor: "#675d5d",
+  primaryColor: 'rgb(239,68,68)',
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
